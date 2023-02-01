90112ed07b592cc75cd9d5183e58a35d6ca1b567
DudeGladiator <66335106+dudegladiator@users.noreply.github.com>
2023-02-02 02:56:44 +0530

    Update main.py

--- main.py
+++ main.py
@@ -67,6 +67,9 @@ if (a):
         
         
         with open("YoutubeVideo.mp4",'rb') as f:
+            video_bytes = f.read()
+
+            st.video(video_bytes)
             st.download_button(label='Save Video', data=f, file_name='YoutubeVideo.mp4',mime="application/octet-stream")
             
     else :
