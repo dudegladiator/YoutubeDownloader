b68574e53edc6acc3a98ea48ee05202a142e7efd
DudeGladiator <66335106+dudegladiator@users.noreply.github.com>
2023-02-02 02:27:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ if (a):
     if option==2:
         
         
-        with open(o, 'wb') as f:
+        with open("YoutubeVideo.mp4", 'wb') as f:
             st.download_button('Save Video', f, file_name='YoutubeVideo.mp4')
             
     else :
