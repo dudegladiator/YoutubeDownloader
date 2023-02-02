3546e271bd0ea041a66c900e1392df46a0aa1e27
DudeGladiator <66335106+dudegladiator@users.noreply.github.com>
2023-02-02 19:13:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,82 +1,109 @@
 import streamlit as st
 from pathlib import Path
-from pytube import YouTube
-import os , time
-import random
+from pytube import YouTube,Playlist
+import os , time,random,sys
 
-def download(link,res,option): 
-    e=time.time()
-    latest_iteration = st.empty()
-    latest_iteration.text(f"{1} Second")
-    bar = st.progress(2)
-    #taking link and res as input
-    # link = input("Enter the Youtube Video URL : ")
-    # res=input ("Resolution e.g-2160p,1440p,4320p etc : ")
-    # option=int(input('''To get Audio - Type 1 \nTo get Video - Type 2 \nTo get Both -'''))
-    #creating a  object of youtube video
+# To Download Any youtube video
+def download(link,res,option,number=1):
     yt = YouTube(link)
-    global k
-    k=str(random.randrange(100))
-
-    audio = yt.streams.get_by_itag(yt.streams.filter(type="audio")[0].itag)
-    
-    
-    a=audio.download()
-    
-    latest_iteration.text(f'{int(time.time()-e)} Second')
-    bar.progress(30)
-    #to change the file name of audio
-    global q
-    q=Path(a)
-   
-    q=q.rename(q.with_name("Audio"+k+".mp3"))
-    q = os.path.abspath(q)
-    global w
-    if (option==2):
-        video = yt.streams.get_by_itag(yt.streams.filter(res=res,type="video")[0].itag)
-        b=video.download()
-        #to change the file name of video
+    #Making title 
+    global title
+    title=""
+    til=yt.title
+    for i in til :
+        if (i==":"or i=="|" or i=="."):
+            title+=" "
+        else: title+=i    
+    #For Audio     File - q   
+    if (option==1):
+        audio = yt.streams.get_by_itag(yt.streams.filter(type="audio")[0].itag)
+        a = audio.download()
+        global q
+        q=Path(a)
+    # For only quality of three types  -  File p   
+    if (res=="720p"or res=="144p"  or res=="360p"):
+        video = yt.streams.get_by_itag(yt.streams.filter(res=res , progressive="True" )[0].itag)
+        
+        v=video.download()
         global p
-        p=Path(b)
-        p=p.rename(p.with_name("Cache"+k+".mp4"))
-        video_file = open("Cache"+k+".mp4", 'rb')
-        video_bytes = video_file.read()
+        p = Path(v)
+    #This can't be run on cloud based  - File k    
+    else :
+        audio = yt.streams.get_by_itag(yt.streams.filter(type="audio")[0].itag)  
+        video = yt.streams.get_by_itag(yt.streams.filter(res=res , type="video" )[0].itag)
         
-        st.video(video_bytes)           
-        latest_iteration.text(f'{int(time.time()-e)} Second')
-        bar.progress(30)
-        #to merge the file 
+        v = video.download()
+        p=Path(v)
+        
+        p = p.rename(p.with_name("CacheVid.mp4"))
+        a = audio.download()
+        q=Path(a)
+        q = q.rename(q.with_name("CacheAud.mp3"))
         import ffmpeg
-        
-        p= os.path.abspath(p)
-        
-        import subprocess 
-        subprocess.run(f"ffmpeg {p} {q} -c:v copy -c:a aac -strict experimental Video{k}.mp4",shell=False)
-        
-        latest_iteration.text(f'{int(time.time()-e)} Second')
-        bar.progress(90)
-    if (option==2):
+        import subprocess
+        subprocess.run(f"ffmpeg -i {p} -i {q} -c copy youtube.mp4")
+        global k
+        k=Path("youtube.mp4")
+        k=k.rename(k.with_name(f"{number} {title}.mp4"))
         os.remove(p)
         os.remove(q)
-    
-    
-    
-    latest_iteration.text(f'{int(time.time()-e)} Second')
-    bar.progress(100)
+
+
+# From here Website start
+
 st.title("Download Youtube Video")
-link=st.text_input("Youtube URL")
-res=st.selectbox("Select The resolution",("720p","144p","240p","360p","480p","720p","1080p","1440p","2160p","4320p"))
-option = st.selectbox("Audio(1) or Video(2)",(1,2))
-a=st.button("Start Downloading")
-if (a):
-    download(link,res,option)
-    if option==2:
+link=st.text_input("Youtube Video or Playlist URL")
+loc=st.radio("Where it is working",("Local","Cloud"))
+#Local
+if (loc=="Local"):
+    #For Playlist
+    if ("playlist" in link):
+        print("Harsh")
+    
+    
+    #For Single video
+    else:
+        option = st.selectbox("Audio(1) or Video(2)",(1,2))
+        #For Video
+        if (option==2):
+            res=st.selectbox("Select The resolution",("720p","144p","240p","360p","480p","1080p","1440p","2160p","4320p"))
+            a=st.button("Start Downloading")
+            if a:
+                download(link,res,option)
+                if (res=="720p"or res=="144p"  or res=="360p"):
+                   with open(p,'rb') as f:
+                        st.download_button(label='Save Video', data=f ,file_name=f"{title}.mp4") 
+                else :
+                    with open(k,'rb') as f:
+                        st.download_button(label='Save Video', data=f ,file_name=f"{title}.mp4")
+        #For Audio
+        else:
+            a=st.button("Start Downloading")
+            if a:
+                download(link, "720p", option)
+                with open(q,'rb' ) as f:
+                    st.download_button("Save Audio",f,file_name=f"{title}.mp3")       
+# cloud                        
+else :
+    #For Playlist    
+    if ("playlist" in link):
+        print("harsh")
         
-        
-        with open(f"Video{k}.mp4",'rb') as f:
-            st.download_button(label='Save Video', data=f, file_name=f'YoutubeVideo{k}.mp4',mime="application/octet-stream")
-            os.remove(f"Video{k}.mp4")
-    else :
-        with open("Audio"+k+".mp3",'rb' ) as f:
-            st.download_button("Save Audio",f,"Music.mp3") 
-        
+    #For single Video
+    else:
+        option = st.selectbox("Audio(1) or Video(2)",(1,2))
+        #For Video
+        if (option==2):
+            res=st.selectbox("Select The resolution",("720p","144p","360p"))
+            a=st.button("Start Downloading")
+            if a:
+                download(link,res,option)
+                with open(p,'rb') as f:
+                    st.download_button(label='Save Video', data=f ,file_name=f"{title}.mp4")
+        #For Audio            
+        else:
+            a=st.button("Start Downloading")
+            if a:
+                download(link, "720p", option)
+                with open(q,'rb' ) as f:
+                    st.download_button("Save Audio",f,file_name=f"{title}.mp3")           
