42c5a1a98c5328dc7e92a80f43f9d2cc8923d85a
DudeGladiator <66335106+dudegladiator@users.noreply.github.com>
2023-02-05 12:23:31 +0530

    For Terminal

---
+++ Youtube_video_downloader.py
@@ -0,0 +1,34 @@
+
+from pathlib import Path
+from pytube import YouTube
+import os
+
+# taking link and res as input
+link = input("Enter the Youtube Video URL : ")
+res=input ("Resolution e.g-2160p,1440p,4320p etc : ")
+option=int(input('''To get Audio - Type 1 \nTo get Video - Type 2 \nTo get Both -  Type 3 \nYour Option No - '''))
+# creating a  object of youtube video
+yt = YouTube(link)
+
+audio = yt.streams.get_by_itag(yt.streams.filter(type="audio")[0].itag)
+print("wait")
+a = audio.download()
+
+ # to change the file name of audio
+q = Path(a)
+q = q.rename(q.with_name("Audio.mp3"))
+if (option == 2):
+        video = yt.streams.get_by_itag(yt.streams.filter(res=res, type="video")[0].itag)
+        b = video.download()
+        # to change the file name of video
+        p = Path(b)
+        p = p.rename(p.with_name("Cache.mp4"))
+
+        # to merge the file
+        import ffmpeg
+        import subprocess
+        subprocess.run(f"ffmpeg -i {p} -i {q} -c copy YoutubeVideo.mp4")
+
+if (option == 2):
+        os.remove(p)
+        os.remove(q)
