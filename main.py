e5106fd8fb3c14a90794091f79b4745a0c1369d6
Harsh Gupta <66335106+dudegladiator@users.noreply.github.com>
2023-04-17 04:00:17 +0530

    Added New Zip system for Playlist

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-
 import zipfile
+from retry import retry
 import streamlit as st
 from pathlib import Path
 from pytube import YouTube,Playlist
 import os , time,random,sys,asyncio
-# To Download Any youtube video
+
+
+# To Download Any youtube video or playlist 
 st.cache()
+@retry(tries=5,delay=2,backoff=2,jitter=(1,3),max_delay=10)
 def download(link,res,option):
     yt = YouTube(link)
     #Making title 
@@ -35,19 +38,18 @@ def download(link,res,option):
     else :
         audio = yt.streams.get_by_itag(yt.streams.filter(type="audio")[0].itag)  
         video = yt.streams.get_by_itag(yt.streams.filter(res=res , type="video" )[0].itag)
-        
+        rand = random.randrange(1,1000)
         v = video.download()
         p=Path(v)
-        
-        p = p.rename(p.with_name("CacheVid.mp4"))
+        p = p.rename(p.with_name(f"{rand}CacheVid.mp4"))
         a = audio.download()
         q=Path(a)
-        q = q.rename(q.with_name("CacheAud.mp3"))
+        q = q.rename(q.with_name(f"{rand}CacheAud.mp3"))
         import ffmpeg
         import subprocess
         subprocess.run(f"ffmpeg -i {p} -i {q} -c copy youtube.mp4")
         global k
-        k=Path("youtube.mp4")
+        k=Path(f"youtube.mp4")
         
         k=k.rename(k.with_name(f"{number} {title}.mp4"))
         os.remove(p)
@@ -65,8 +67,8 @@ global number
 number=random.randrange(1,1000)
 link=st.text_input("Youtube Video or Playlist URL")
 
-loc=st.radio("Where it is working",("On Cloud(Using Website Version)","Local Server(Highest Quality Available)"))
-
+#loc=st.radio("Where it is working",("On Cloud(Using Website Version)","Local Server(Highest Quality Available)"))
+loc="Cloud"
 #Local
 if (loc=="Local Server(Highest Quality Available)"):
     #For Playlist
@@ -81,46 +83,49 @@ if (loc=="Local Server(Highest Quality Available)"):
         if (option==2):
                 res=st.selectbox("Select The resolution",("720p","144p","240p","360p","480p","1080p","1440p","2160p","4320p"))
                 a=st.button("Start Downloading")
-                if "load_state" not in st.session_state:
-                    st.session_state.load_state = False
-                if a or st.session_state.load_state:
-                    st.session_state.load_state=True
-                    
+                k=random.randrange(1,1000)
+                if a:
                     for i , url in enumerate(ytplay.video_urls,random.randrange(1,1000)):
                         number=i
-                        try : 
+                        try :
                             download(url,res,option)
                         except IndexError:
-                            st.write("Resolution not available")
-                            st.stop()
-                        except :
-                            st.write("Try Again")
-                            st.experimental_rerun()    
-                        st.write(f"{number} {title}")
-                        if (res=="720p"or res=="144p"  or res=="360p"):
-                            st.write("Automatically Saved in opened Folder or in your admisntration Folder")
-                        else :
-                            st.write("Automatically Saved in opened Folder or in your admisntration Folder")
-                                
-                                
-                                
+                            st.write("You have selected resolution which is not available for all videos")
+                            st.write("Try with other resolution")
+                            st.stop()    
+                        with zipfile.ZipFile(f"{k} Youtube videos.zip", mode="a") as archive:
+                            archive.write(p)
+                    try:        
+                        with open(f"{k} Youtube videos.zip",'rb') as f:
+                            st.write("Downloaded Successfully")
+                            st.download_button(label='Save File', data=f ,file_name=f"{k} Youtube videos.zip")            
+                    except :
+                        st.write("Error Can be due to following reasons : ")
+                        st.write("    1. You have selected resolution which is not available for all videos")
+                        st.write("    2. File is too large to download")
+                        st.write("    3. Some Videos are not avaialble to download")
+                        st.write("Check the link")
+                        st.write("Try Again")
+                        st.stop()
+                          
             #For Audio
         else:
                 a=st.button("Start Downloading") 
-                if "load_state" not in st.session_state:
-                    st.session_state.load_state = False
-                if a or st.session_state.load_state:
-                    st.session_state.load_state=True
+                k=random.randrange(1,1000)
+                if a :
                     for i , url in enumerate(ytplay.video_urls,random.randrange(1,1000)):
                         number=i
                         try:
-                            download(url,"360p", option)
-                        except :
+                            download(url,"360p",option)    
+                        except:
                             st.write("Try Again")
-                            st.experimental_rerun()
-                        
-                        st.write(f"{number} {title}")
-                        st.write("Automatically Saved in opened Folder or in your admisntration Folder")
+                            st.stop()
+                        with zipfile.ZipFile(f"{k} Youtube Songs.zip", mode="a") as archive:
+                            archive.write(q)
+                    with open(f"{k} Youtube Songs.zip",'rb') as f:
+                        st.write("Downloaded Successfully")
+                        st.download_button(label='Save File', data=f ,file_name=f"{k} Youtube Songs.zip")            
+                    
                  
                               
     #For Single video
@@ -132,33 +137,39 @@ if (loc=="Local Server(Highest Quality Available)"):
             a=st.button("Start Downloading")
             if a:
                 number+=1
-                try : 
+                try :
                     download(link,res,option)
                 except IndexError:
-                    st.write("Resolution not available")
-                    st.stop()
+                    st.write("You have selected resolution which is not available for all videos")
+                    st.write("Try with other resolution")
+                    st.stop()    
+                try:  
+                    with open(p,'rb') as f:
+                        st.write(f"{number} {title}") 
+                        st.download_button(label='Save File', data=f ,file_name=f"{number} {title}.mp4")            
                 except :
+                    st.write("Error Can be due to following reasons : ")
+                    st.write("    1. You have selected resolution which is not available for all videos")
+                    st.write("    2. File is too large to download")
+                    st.write("    3. Some Videos are not avaialble to download")
+                    st.write("Check the link")
                     st.write("Try Again")
-                    st.experimental_rerun()    
-                        
-                st.write(f"{number} {title}")
-                if (res=="720p"or res=="144p"  or res=="360p"):
-                   st.write("Automatically Saved in opened Folder or in your admisntration Folder") 
-                else :
-                    st.write("Automatically Saved in opened Folder or in your admisntration Folder")
+                    st.stop()
         #For Audio
         else:
             a=st.button("Start Downloading")
             if a:
                 number+=1
                 try:
-                    download(link, "360p", option)
+                    download(link,"360", option)
                 except:
                     st.write("Try Again")
-                    st.experimental_rerun()
-                st.write(f"{number} {title}")
-                st.write("Automatically Saved in opened Folder or in your admisntration Folder")     
-                      
+                    st.stop()
+                
+                with open(q,'rb' ) as f:
+                    st.write(f"{number} {title}")
+                    st.download_button("Save Audio",f,file_name=f"{number} {title}.mp3")   
+                         
 # cloud                        
 else :
     #For Playlist    
@@ -175,46 +186,49 @@ else :
         if (option==2):
                 res=st.selectbox("Select The resolution",("720p","144p","360p"))
                 a=st.button("Start Downloading")
-                if "load_state" not in st.session_state:
-                    st.session_state.load_state = False
-                if a or st.session_state.load_state:
-                    st.session_state.load_state=True
-                    k= random.randrange(1,1000)   
-                    for i , url in enumerate(ytplay.video_urls,random.randrange(1,1000)):
-                        number=i
-                        try : 
-                            download(url,res,option)
-                        except IndexError:
-                            st.write("Resolution not available")
-                            st.stop()
-                        except :
-                            st.write("Try Again")
-                            st.experimental_rerun()    
-                            
-                        st.write(f"{number} {title}")
-                         
-                        with zipfile.ZipFile(f"{k} Youtube videos.zip", mode="a") as archive:
-                            archive.write(p)
-                   with open(f"{k} Youtube videos.zip",'rb') as f:
-                            st.download_button(label='Save File', data=f ,file_name=f"{k} Youtube videos.zip")         
-            #For Audio
+                k= random.randrange(1,1000)   
+                for i , url in enumerate(ytplay.video_urls,random.randrange(1,1000)):
+                    number=i
+                    download(url,res,option)
+                    with zipfile.ZipFile(f"{k} Youtube videos.zip", mode="a") as archive:
+                        archive.write(p)
+                try:    
+                    with open(f"{k} Youtube videos.zip",'rb') as f:
+                        st.write("Downloaded Successfully")
+                        st.download_button(label='Save File', data=f ,file_name=f"{k} Youtube videos.zip")            
+                except :
+                    st.write("Error Can be due to following reasons : ")
+                    st.write("    1. You have selected resolution which is not available for all videos")
+                    st.write("    2. File is too large to download")
+                    st.write("    3. Some Videos are not avaialble to download")
+                    st.write("Check the link")
+                    st.write("Try Again")
+                    st.stop()
+        #For Audio
         else:
                 a=st.button("Start Downloading")  
-                if "load_state" not in st.session_state:
-                    st.session_state.load_state = False
-                if a or st.session_state.load_state:
-                    st.session_state.load_state=True
+                k=random.randrange(1,1000)
+                if a :
                     for i , url in enumerate(ytplay.video_urls,random.randrange(1,1000)):
                         number=i
                         try:
-                            download(url, "360p", option)
-                        except :
+                            download(url,"360p",option)    
+                        except:
                             st.write("Try Again")
-                            st.experimental_rerun()
-                            
-                        st.write(f"{number} {title}")
-                        with open(q,'rb' ) as f:
-                            st.download_button("Save Audio",f,file_name=f"{i} {title}.mp3")
+                            st.stop()
+                        with zipfile.ZipFile(f"{k} Youtube Songs.zip", mode="a") as archive:
+                            archive.write(q)
+                    try:      
+                        with open(f"{k} Youtube Songs.zip",'rb') as f:
+                            st.write("Downloaded Successfully")
+                            st.download_button(label='Save File', data=f ,file_name=f"{k} Youtube Songs.zip")            
+                    except :
+                        st.write("Error Can be due to following reasons : ")
+                        st.write("    1. File is too large to download")
+                        st.write("    2. Some Songs are not avaialble to download")
+                        st.write("Check the link")
+                        st.write("Try Again")
+                        st.stop()
 
     #For single Video
     else:
@@ -225,17 +239,25 @@ else :
             a=st.button("Start Downloading")
             if a:
                 number+=1
-                try:
-                    download(link,res, option)
-                except IndexError:
-                    st.write("Resolution not available")
-                    st.stop()
+                try :
+                    download(link,res,option)
                 except :
+                    st.write("You have selected resolution which is not available for all videos")
+                    st.write("Try again")
+                    st.stop()    
+                try:
+                          
+                    with open(p,'rb') as f:
+                        st.write(f"{number} {title}") 
+                        st.download_button(label='Save File', data=f ,file_name=f"{number} {title}.mp4")            
+                except :
+                    st.write("Error Can be due to following reasons : ")
+                    st.write("    1. You have selected resolution which is not available for all videos")
+                    st.write("    2. File is too large to download")
+                    st.write("    3. Some Videos are not avaialble to download")
+                    st.write("Check the link")
                     st.write("Try Again")
-                    st.experimental_rerun() 
-                st.write(f"{number} {title}")
-                with open(p,'rb') as f:
-                    st.download_button(label='Save Video', data=f ,file_name=f"{title}.mp4")
+                    st.stop()
         #For Audio            
         else:
             a=st.button("Start Downloading")
@@ -244,11 +266,13 @@ else :
                 try:
                     download(link,"360", option)
                 except:
+                    st.write("Check the link")
                     st.write("Try Again")
-                    st.experimental_rerun()
+                    
+                    st.stop()
                 st.write(f"{number} {title}")
                 with open(q,'rb' ) as f:
-                    st.download_button("Save Audio",f,file_name=f"{title}.mp3")           
+                    st.download_button("Save Audio",f,file_name=f"{number} {title}.mp3")           
                     
 "Follow Me On Github - https://github.com/dudegladiator" 
                    
